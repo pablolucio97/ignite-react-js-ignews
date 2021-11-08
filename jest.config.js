f3a79fb7ba63f604d130040b40d0a92ae12b274f
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-11-08 07:48:05 -0300

    Adding tests coverage

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,21 @@
 module.exports={
     testPathIgnorePatterns: ["/node_modules", "/.next"],
     setupFilesAfterEnv: ["<rootDir>/src/tests/setupTest.ts"],
-    trasform: {
+    transform: {
         "^.+\\.(js|jsx|ts|tsx)$" : "<rootDir>/node_modules/babel-jest"
     },
     moduleNameMapper: {
         "\\.(css|scss|saas)$" : "identity-obj-proxy"
     },
-    testEnvironment: 'jsdom'
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/tests/**/*.tsx",
+        "!src/tests/**/*.spec.tsx",
+        "!src/**/_app.tsx",
+        "!src/**/_document.tsx"
+    ],
+    coverageReporters: [
+        "lcov",
+        "json"
+    ]
 }
